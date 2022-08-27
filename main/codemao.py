e583d4cb575ff52a0effeef5dba8d8720a6b29b9
technology-me <lishangmingxuan@foxmail.com>
2022-08-27 23:02:35 +0800

    Upload server API

--- main/codemao.py
+++ main/codemao.py
@@ -3,11 +3,11 @@ from typing import Optional
 
 import requests
 
-headers = {
-    "Content-Type": "application/json",
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.60 '
-                  'Safari/537.17 '
-}
+# headers = {
+#     "Content-Type": "application/json",
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.60 '
+#                   'Safari/537.17 '
+# }
 
 
 def login(username: str, password: str) -> Optional[str]:
@@ -17,12 +17,14 @@ def login(username: str, password: str) -> Optional[str]:
     :param password: 向服务器请求的密码
     :return: 如果成功返回id，失败返回 None
     """
-
-    value = requests.get(url="https://api.xyfish.cn/api/coco_login/api.php?"+'user='+username+'&pwd='+password,
-                          headers=headers).text
-    print(value)
+    data = {
+        'user': username,
+        'pwd': password
+    }
+    value = requests.post(url="https://api.bcmcreator.cn/login/user_bcmlogin.php",
+                          data=data).text
+    print(username, password, value)
     if 'id' in json.loads(value).keys():
         return str(json.loads(value)['id'])
-
     else:
         return None
